a198c89043b0edf23f061b2fbf8c85c3d0304097
Nanoo <45440100+Calemy@users.noreply.github.com>
2026-06-28 16:41:45 +0200

    feat: performance

--- src/performance.zig
+++ src/performance.zig
@@ -4,6 +4,7 @@ const std = @import("std");
 const Beatmap = @import("beatmap.zig");
 const Difficulty = @import("difficulty.zig");
 const Score = @import("score.zig");
+const Mods = @import("mods.zig").Mods;
 const rosu = @import("pp.zig");
 
 const Handle = c.rosu_pp_PerformanceHandle;
@@ -30,6 +31,11 @@ pub const Attributes = extern struct {
     }
 };
 
+pub const Priority = enum(u32) {
+    BestCase = 0,
+    WorstCase = 1,
+};
+
 pub const Performance = struct {
     const Self = @This();
     handle: ?*Handle,
@@ -40,6 +46,111 @@ pub const Performance = struct {
 
     pub fn free(self: Self) void {
         c.rosu_pp_performance_free(self.handle);
+    }
+
+    pub fn mods(self: Self, value: Mods) Performance {
+        _ = c.rosu_pp_performance_mods(self.handle, value.handle);
+        return self;
+    }
+
+    pub fn objects(self: Self, value: u32) Performance {
+        _ = c.rosu_pp_performance_passed_objects(self.handle, value);
+        return self;
+    }
+
+    pub fn clockRate(self: Self, value: f64) Performance {
+        _ = c.rosu_pp_performance_clock_rate(self.handle, value);
+        return self;
+    }
+
+    pub fn ar(self: Self, value: f32, fixed: bool) Performance {
+        _ = c.rosu_pp_performance_ar(self.handle, value, fixed);
+        return self;
+    }
+
+    pub fn cs(self: Self, value: f32, fixed: bool) Performance {
+        _ = c.rosu_pp_performance_cs(self.handle, value, fixed);
+        return self;
+    }
+
+    pub fn hp(self: Self, value: f32, fixed: bool) Performance {
+        _ = c.rosu_pp_performance_hp(self.handle, value, fixed);
+        return self;
+    }
+
+    pub fn od(self: Self, value: f32, fixed: bool) Performance {
+        _ = c.rosu_pp_performance_od(self.handle, value, fixed);
+        return self;
+    }
+
+    pub fn hardrockOffset(self: Self, value: bool) Performance {
+        _ = c.rosu_pp_performance_hardrock_offsets(self.handle, value);
+        return self;
+    }
+
+    pub fn lazer(self: Self, value: bool) Performance {
+        _ = c.rosu_pp_performance_lazer(self.handle, value);
+        return self;
+    }
+
+    pub fn accuracy(self: Self, value: f64) Performance {
+        _ = c.rosu_pp_performance_ar(self.handle, value);
+        return self;
+    }
+
+    pub fn misses(self: Self, value: u32) Performance {
+        _ = c.rosu_pp_performance_misses(self.handle, value);
+        return self;
+    }
+
+    pub fn combo(self: Self, value: u32) Performance {
+        _ = c.rosu_pp_performance_combo(self.handle, value);
+        return self;
+    }
+
+    pub fn largeTickHits(self: Self, value: u32) Performance {
+        _ = c.rosu_pp_performance_large_tick_hits(self.handle, value);
+        return self;
+    }
+
+    pub fn smallTickHits(self: Self, value: u32) Performance {
+        _ = c.rosu_pp_performance_small_tick_hits(self.handle, value);
+        return self;
+    }
+
+    pub fn sliderEndHits(self: Self, value: u32) Performance {
+        _ = c.rosu_pp_performance_slider_end_hits(self.handle, value);
+        return self;
+    }
+
+    pub fn n300(self: Self, value: u32) Performance {
+        _ = c.rosu_pp_performance_n300(self.handle, value);
+        return self;
+    }
+
+    pub fn n100(self: Self, value: u32) Performance {
+        _ = c.rosu_pp_performance_n100(self.handle, value);
+        return self;
+    }
+
+    pub fn n50(self: Self, value: u32) Performance {
+        _ = c.rosu_pp_performance_n50(self.handle, value);
+        return self;
+    }
+
+    pub fn legacyScore(self: Self, value: u32) Performance {
+        _ = c.rosu_pp_performance_legacy_total_score(self.handle, value);
+        return self;
+    }
+
+    pub fn hitPriority(self: Self, value: Priority) Performance {
+        _ = c.rosu_pp_performance_hitresult_priority(self.handle, value);
+        return self;
+    }
+
+    pub fn state(self: Self, score: Score.State) Performance {
+        _ = c.rosu_pp_performance_state(self.handle, @ptrCast(&score));
+        return self;
     }
 
     /// If called you no longer require to deinit.
@@ -55,11 +166,6 @@ pub const Performance = struct {
         const result: FFIResult = @enumFromInt(Handle.rosu_pp_performance_checked_calculate(self.handle, @ptrCast(&attr)));
         if (result == .TooSuspicious) return error.TooSuspicious;
         return attr;
-    }
-
-    pub fn state(self: Self, score: Score.State) Performance {
-        _ = c.rosu_pp_performance_state(self.handle, @ptrCast(&score));
-        return self;
     }
 };
 
